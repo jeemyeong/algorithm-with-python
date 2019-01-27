aa617b39adf82c4899ebf5727e396974bbd08532
jeemyeong <jeemyeong@gmail.com>
2019-01-28 01:53:35 +0900

    Refactor quicksort

--- quicksort/quicksort.py
+++ quicksort/quicksort.py
@@ -1,12 +1,12 @@
-def partition(nums, low, high):
+def partition1(nums, low, high):
     pivot = low
-    low = low + 1
+    low += 1
     while low <= high:
-        if nums[high] >= nums[pivot]:
-            high -= 1
-            continue
         if nums[low] <= nums[pivot]:
             low += 1
+            continue
+        if nums[high] >= nums[pivot]:
+            high -= 1
             continue
         nums[high], nums[low] = nums[low], nums[high]
         low += 1
@@ -14,13 +14,29 @@ def partition(nums, low, high):
     nums[pivot], nums[high] = nums[high], nums[pivot]
     return high
 
-def helper(nums, low, high):
+def partition2(nums, low, high):
+    pivot = high
+    high -= 1
+    while low <= high:
+        if nums[low] <= nums[pivot]:
+            low += 1
+            continue
+        if nums[high] >= nums[pivot]:
+            high -= 1
+            continue
+        nums[high], nums[low] = nums[low], nums[high]
+        low += 1
+        high -= 1
+    nums[pivot], nums[low] = nums[low], nums[pivot]
+    return low
+
+def helper(nums, low, high, partition):
     if low >= high:
         return
     middle = partition(nums, low, high)
-    helper(nums, low, middle-1)
-    helper(nums, middle+1, high)
+    helper(nums, low, middle-1, partition)
+    helper(nums, middle+1, high, partition)
 
-def sort(nums):
-    helper(nums, 0, len(nums)-1)
+def sort(nums, partition):
+    helper(nums, 0, len(nums)-1, partition)
     return nums
--- quicksort/test_quicksort.py
+++ quicksort/test_quicksort.py
@@ -1,11 +1,19 @@
-from quicksort import sort
+from quicksort import sort, partition1, partition2
 
 def test_sort():
-    assert sort([5,3,7,6,1,2,4]) == [1,2,3,4,5,6,7]
-    assert sort([1,2,3,4,5,6,7]) == [1,2,3,4,5,6,7]
-    assert sort([1,1]) == [1,1]
-    assert sort([3,2,1]) == [1,2,3]
-    assert sort([3,1,2]) == [1,2,3]
-    assert sort([4,1,2,3]) == [1,2,3,4]
-    assert sort([1]) == [1]
-    assert sort([]) == []
+    assert sort([5,3,7,6,1,2,4], partition1) == [1,2,3,4,5,6,7]
+    assert sort([5,3,7,6,1,2,4], partition2) == [1,2,3,4,5,6,7]
+    assert sort([1,2,3,4,5,6,7], partition1) == [1,2,3,4,5,6,7]
+    assert sort([1,2,3,4,5,6,7], partition2) == [1,2,3,4,5,6,7]
+    assert sort([1,1], partition1) == [1,1]
+    assert sort([1,1], partition2) == [1,1]
+    assert sort([3,2,1], partition1) == [1,2,3]
+    assert sort([3,2,1], partition2) == [1,2,3]
+    assert sort([3,1,2], partition1) == [1,2,3]
+    assert sort([3,1,2], partition2) == [1,2,3]
+    assert sort([4,1,2,3], partition1) == [1,2,3,4]
+    assert sort([4,1,2,3], partition2) == [1,2,3,4]
+    assert sort([1], partition1) == [1]
+    assert sort([1], partition2) == [1]
+    assert sort([], partition1) == []
+    assert sort([], partition2) == []
